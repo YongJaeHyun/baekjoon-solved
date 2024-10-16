c61bf27fe9e45141c8e7ba297e5b360c2c15dcac
Huze <dltjrrbs2020@gmail.com>
2024-10-16 22:09:26 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 44.32 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,8 +1,11 @@
+// 이분 탐색 기법 활용
+
 function solution(diffs, times, limit) {
     let level = 50000;
     let leftLevel = 1;
     let rightLevel = 100000;
     
+    // 해당 숙련도로 limit이 넘는지 확인
     function getIsLevelExceedLimit(level) {
         let totalTime = 0;
         
@@ -18,23 +21,26 @@ function solution(diffs, times, limit) {
                 totalTime += repeatCnt * (prevTime + currentTime) + currentTime;
             }
             
-            if(totalTime > limit) return false;
+            if(totalTime > limit) return true;
         }
-        return true;
+        return false;
     }
     
     while(true){
         const isExceedLimit = getIsLevelExceedLimit(level)
         
+        // limit을 넘을 경우, 현재 숙련도(level)가 부족하다는 것이므로, 중앙 기준 왼쪽 범위를 무시
         if(isExceedLimit){
-            if(rightLevel - leftLevel <= 1) return leftLevel;
-            
-            rightLevel = level;
-            level = Math.floor((leftLevel + rightLevel) / 2)
-        } else {
             if(rightLevel - leftLevel <= 1) return rightLevel;
             
             leftLevel = level;
+            level = Math.floor((leftLevel + rightLevel) / 2)
+        }
+        // 넘지 않을 경우, 중앙 기준 오른쪽 범위를 무시
+        else {
+            if(rightLevel - leftLevel <= 1) return level;
+            
+            rightLevel = level;
             level = Math.floor((leftLevel + rightLevel) / 2)
         }
     }
