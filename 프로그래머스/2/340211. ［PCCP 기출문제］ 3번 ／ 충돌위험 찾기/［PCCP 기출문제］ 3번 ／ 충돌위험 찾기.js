05c661508f0bd22b6dc6e7e65614a6deff09fb0d
Huze <dltjrrbs2020@gmail.com>
2024-10-18 02:34:32 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 219.81 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.js
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.js
@@ -1,45 +1,52 @@
 function solution(points, routes) {
-  var answer = 0;
-  if (routes.length > 0 && points.length > 0) {
-    let moves = new Array(routes.length).fill().map(() => []);
-    for (let i = 0; i < routes.length; i++) {
-      for (let j = 0; j < routes[0].length - 1; j++) {
-        const start = points[routes[i][j] - 1];
-        const end = points[routes[i][j + 1] - 1];
-        const moveDistance = [end[0] - start[0], end[1] - start[1]];
-        for (let k = j === 0 ? 0 : 1; k <= Math.abs(moveDistance[0]); k++) {
-          moves[i].push(
-            moveDistance[0] < 0
-              ? [start[0] - k, start[1]]
-              : [start[0] + k, start[1]]
-          );
+    let answer = 0;
+    const map = new Map();
+    
+    // 시작 시간에 충돌 체크
+    let runners = routes.map(([startPointNum, ...endPointNums]) => {
+        const startPoint = points[startPointNum - 1];
+        const isCollide = map.has(startPointNum);
+        map.set(startPointNum, isCollide ? map.get(startPointNum) + 1 : 1);
+        return { position: [...startPoint], endPointNums };
+    });
+    
+    // 충돌 수 세기
+    answer += Array.from(map.values()).filter(value => value > 1).length;
+
+    while (true) {
+        const newMap = new Map();
+        let isAllEnd = true;
+
+        for (let runner of runners) {
+            let { position, endPointNums } = runner;
+            if (endPointNums.length === 0) continue;
+
+            let [x, y] = position;
+            const currentEndPointIdx = endPointNums[0] - 1;
+            const [endX, endY] = points[currentEndPointIdx];
+
+            if (x !== endX) {
+                isAllEnd = false;
+                x += (x < endX) ? 1 : -1;
+            } else if (y !== endY) {
+                isAllEnd = false;
+                y += (y < endY) ? 1 : -1;
+            }
+            runner.position = [x, y]
+
+            // 도착하면 경유지 제거
+            if (x === endX && y === endY) {
+                endPointNums.shift();
+            }
+
+            // 새로운 위치 체크
+            const key = `${x},${y}`;
+            newMap.set(key, (newMap.get(key) || 0) + 1);
         }
-        for (let k = 1; k <= Math.abs(moveDistance[1]); k++) {
-          moves[i].push(
-            moveDistance[1] < 0
-              ? [end[0], start[1] - k]
-              : [end[0], start[1] + k]
-          );
-        }
-      }
+
+        // 충돌 수 세기
+        answer += Array.from(newMap.values()).filter(value => value > 1).length;
+
+        if (isAllEnd) return answer;
     }
-    const max = Math.max(...moves.map((item) => item.length));
-    for (let i = 0; i < max; i++) {
-      let map = [...new Array(101)].map(() => [...new Array(101).fill(0)]);
-      for (let j = 0; j < routes.length; j++) {
-        if (moves[j][i]) {
-          const [r, c] = moves[j][i];
-          if (map[r][c] >= 2) {
-            continue;
-          } else if (map[r][c] === 1) {
-            answer++;
-            map[r][c]++;
-          } else {
-            map[r][c] = 1;
-          }
-        }
-      }
-    }
-  }
-  return answer;
 }
