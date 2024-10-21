0f3b96cb62122407da59d0dcf9ec7cc6a45c3400
Huze <dltjrrbs2020@gmail.com>
2024-10-21 23:51:16 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,3 +1,5 @@
+// 시뮬레이션
+
 function solution(video_len, pos, op_start, op_end, commands) {
     let [opStartMinute, opStartSecond] = op_start.split(":").map(num => parseInt(num));
     let [opEndMinute, opEndSecond] = op_end.split(":").map(num => parseInt(num));
@@ -5,13 +7,13 @@ function solution(video_len, pos, op_start, op_end, commands) {
     
     function executeCommand(command){   
         let [minute, second] = pos.split(":").map(num => parseInt(num));
+       
         command === "next" ? second += 10 : second -= 10;
-        
         if(second >= 60) {
-            minute += 1;
+            minute++;
             second -= 60;
         } else if (second < 0) {
-            minute -= 1;
+            minute--
             second += 60;
         }
         
@@ -47,11 +49,3 @@ function solution(video_len, pos, op_start, op_end, commands) {
     
     return pos;
 }
-    
-
-
-//     const [leftHour, leftMin] = left.split(":").map(parseInt);
-//     const [rightHour, rightMin] = right.split(":").map(parseInt);
-    
-//     if(leftHour > rightHour) return true;
-//     else if(leftHour === rightHour && leftMin > rightMin) return true;
