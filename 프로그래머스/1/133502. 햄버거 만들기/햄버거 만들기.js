bbf88c66e616fffa19d7254f1e234c07d2bd13da
Huze <dltjrrbs2020@gmail.com>
2024-10-25 22:13:24 +0900

    [level 1] Title: 햄버거 만들기, Time: 291.68 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,27 @@
+function slice(array, start, end=array.length){
+    let sliced = [];
+    if(start < 0) start = array.length + start;
+    if(end < 0) end = array.length + end;
+    
+    for(let i = start; i < end; i++){
+        sliced.push(array[i]);
+    }
+    return sliced;
+}
+
+function solution(ingredient) {
+    let answer = 0;
+    let stack = []
+    for(let i = 0; i < ingredient.length; i++){
+        stack.push(ingredient[i])
+        if(stack.length >= 4){
+            const currentIngredient = slice(stack, -4).join('')
+            if(currentIngredient === '1231'){
+                stack.length -= 4; // 배열 마지막 4개 요소 삭제
+                answer++
+            }
+        }
+        
+    }
+    return answer;
+}
