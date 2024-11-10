36ef65b3a10ff76126a192afdfbc4e3d23375630
Huze <dltjrrbs2020@gmail.com>
2024-11-11 01:56:23 +0900

    [level 3] Title: 야근 지수, Time: 889.95 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,18 @@
+function solution(n, works) {
+    works.sort((a, b) => b - a);
+
+    let i = 0;
+    while (n > 0) {
+        if (works[i] === 0) break;
+
+        works[i]--;
+        n--;
+        while (i < works.length - 1 && works[i] < works[i + 1]) {
+            [works[i], works[i + 1]] = [works[i + 1], works[i]];
+            i++;
+        }
+        i = 0;
+    }
+
+    return works.reduce((acc, curr) => acc + curr ** 2, 0);
+}
