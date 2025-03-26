0ebea9bee4d76cd5959fffde30b6322ea23b83a6
Huze <dltjrrbs2020@gmail.com>
2025-03-26 11:48:44 +0900

    [level 1] Title: 유연근무제, Time: 3.02 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -0,0 +1,26 @@
+function solution(schedules, timelogs, startday) {
+    let answer = 0;
+    for(let i = 0; i < schedules.length; i++){
+        const schedule = schedules[i];
+        let hopeSchedule = schedule + 10;
+        let isSuccess = true;
+        for(let j = 0; j < 7; j++){
+            if((startday + j) % 7 === 6 || (startday + j) % 7 === 0) continue;
+            let timelog = timelogs[i][j];
+            if(hopeSchedule % 100 >= 60) {
+                hopeSchedule += 40 
+            }
+            if(timelog / 100 === 0){
+                timelog += 2400
+            }
+            
+            if(hopeSchedule < timelog){
+                isSuccess = false;
+                break;
+            }
+        }
+        
+        if(isSuccess) answer++
+    }
+    return answer;
+}
