4e181f433f0094e0e1c238e6b80cf6bff3727323
Huze <dltjrrbs2020@gmail.com>
2025-04-12 00:27:47 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.20 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -1,21 +1,19 @@
 function solution(players, m, k) {
     let answer = 0;
-    let servers = [];
-    
-    for(let time = 0; time < 24; time++){
-        servers = servers.filter(({ end }) => time < end);
-        
-        const currPlayers = players[time];
-        const requireServerLength = Math.floor(currPlayers / m);
-        
-        if(requireServerLength > servers.length){
-            let addServerCnt = requireServerLength - servers.length;
-            while(addServerCnt--){
-                servers.push({ start: time, end: time + k });
-                answer += 1;
-            }
+    let activeServers = [];
+
+    for (let hour = 0; hour < 24; hour++) {
+        activeServers = activeServers.filter(server => server.end > hour);
+
+        const playerCount = players[hour];
+        const neededServers = Math.floor(playerCount / m);
+
+        const additionalServers = neededServers - activeServers.length;
+        for (let i = 0; i < additionalServers; i++) {
+            activeServers.push({ start: hour, end: hour + k });
+            answer++;
         }
     }
-    
+
     return answer;
 }
