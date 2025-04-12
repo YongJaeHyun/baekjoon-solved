35232c72aa92c77327855b2c0e0e221fe1f6bbf4
Huze <dltjrrbs2020@gmail.com>
2025-04-13 01:09:03 +0900

    [level 2] Title: 완전범죄, Time: 53.02 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -0,0 +1,21 @@
+function solution(info, n, m) {
+    const dp = Array.from({ length: info.length + 1 }, () => new Set());
+    dp[0].add("0,0");
+    
+    for(let i = 1; i < dp.length; i++) {
+        for(let prevInfo of dp[i - 1]) {
+            const [prevAInfo, prevBInfo] = prevInfo.split(",").map(Number);
+            const [aInfo, bInfo] = info[i - 1];
+            if(prevAInfo + aInfo < n) {
+                dp[i].add(`${prevAInfo + aInfo},${prevBInfo}`);
+            } 
+            if(prevBInfo + bInfo < m) {
+                dp[i].add(`${prevAInfo},${prevBInfo + bInfo}`);
+            }
+        }
+    }
+    
+    const lastInfo = Array.from(dp.at(-1)).map((info) => info.split(",").map(Number));
+    if(lastInfo.length === 0) return -1;
+    return lastInfo.sort((a, b) => a[0] - b[0])[0][0];
+}
